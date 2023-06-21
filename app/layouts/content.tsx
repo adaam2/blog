e86a9524d64f49ed4d51f24b2e76a8b778c5ccbd
Adam Bull <adam@keel.xyz>
2023-06-21 10:02:32 +0100

    margin

--- app/layouts/content.tsx
+++ app/layouts/content.tsx
@@ -9,10 +9,12 @@ const El = styled.div`
   max-width: 960px;
   margin: 0 auto;
   padding: 1em;
+  margin-bottom: 150px;
 
   @media screen and (max-width: 960px) {
     max-width: 900px;
     margin: 0 40px;
+    margin-bottom: 100px;
   }
 `;
 
